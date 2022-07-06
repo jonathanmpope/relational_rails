c53dc48f140de086339435dd1273259186ef0192
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-06 15:22:45 -0600

    feat: add lesson format feature and test

--- spec/features/lessons/index_spec.rb
+++ spec/features/lessons/index_spec.rb
@@ -10,9 +10,19 @@ RSpec.describe 'lesson index page' do
         lesson1 = course1.lessons.create!(name:"Thinking about thinking", format:"text", questions:3, complete: true)
 
         visit '/lessons'
-        save_and_open_page
 
         expect(page).to have_content(lesson1.name)
         expect(page).to have_content("Name: #{lesson1.name}")
+    end 
+
+    it 'can show the format' do 
+        course1 = Course.create!(name: "Basics", participants: 20, complete: false)
+        lesson1 = course1.lessons.create!(name:"Thinking about thinking", format:"text", questions:3, complete: true)
+
+        visit '/lessons'
+        save_and_open_page
+
+        expect(page).to have_content(lesson1.format)
+        expect(page).to have_content("Format: #{lesson1.format}")
     end 
 end 
