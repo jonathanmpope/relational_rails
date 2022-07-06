7ce8a548c8546f7c97853c7473dcc164279906e8
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-06 14:24:42 -0600

    Feat: course index show participants

--- spec/features/courses/index_spec.rb
+++ spec/features/courses/index_spec.rb
@@ -13,8 +13,17 @@ RSpec.describe "Courses index page", type: :feature do
         visit '/courses'
 
         expect(page).to have_content(course1.name)
-        # expect(page).to have_content("Participants: #{course1.participants}")
         expect(page).to have_content("Name: #{course2.name}")
-        # expect(page).to have_content("Participants: #{course2.participants}")
+    end 
+
+    it 'can see the number of participants each course has' do 
+        course1 = Course.create!(name: "Basics", participants: 20, complete: false)
+        course2 = Course.create!(name: "Listening", participants: 12, complete: false)
+
+        visit '/courses'
+        save_and_open_page
+
+        expect(page).to have_content(course1.participants)
+        expect(page).to have_content("Participants: #{course2.participants}")
     end 
 end 
