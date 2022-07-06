07ad485ba1b3501fb1cf741df862c9df1f3bc03e
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-06 16:04:07 -0600

    feat: add course lesson questions feat with test

--- spec/features/courses/lessons/index_spec.rb
+++ spec/features/courses/lessons/index_spec.rb
@@ -29,12 +29,27 @@ RSpec.describe 'Course lesson index page' do
         lesson4 = course1.lessons.create!(name:"Quit tomorrow", format:"video", questions:1, complete: false)
 
         visit "/courses/#{course1.id}/lessons"
-        save_and_open_page
 
         expect(page).to have_content(lesson1.format)
         expect(page).to have_content(lesson2.format)
         expect(page).to have_content(lesson3.format)
         expect(page).to have_content(lesson4.format)
+    end 
+
+    it 'has a list of lesson questions on the page' do
+        course1 = Course.create!(name: "Basics", participants: 20, complete: false)
+        lesson1 = course1.lessons.create!(name:"Thinking about thinking", format:"text", questions:3, complete: true)
+        lesson2 = course1.lessons.create!(name:"Attention", format:"text", questions:3, complete: true)
+        lesson3 = course1.lessons.create!(name:"Trying is lying", format:"text", questions:2, complete: false)
+        lesson4 = course1.lessons.create!(name:"Quit tomorrow", format:"video", questions:1, complete: false)
+
+        visit "/courses/#{course1.id}/lessons"
+        save_and_open_page
+
+        expect(page).to have_content(lesson1.questions)
+        expect(page).to have_content(lesson2.questions)
+        expect(page).to have_content(lesson3.questions)
+        expect(page).to have_content(lesson4.questions)
     end 
 
 end 
