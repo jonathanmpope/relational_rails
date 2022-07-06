34b1f2b83dadebe63f6f304fc23ed345b16b8d77
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-06 15:24:40 -0600

    feat: add lesson questions feature and test

--- spec/features/lessons/index_spec.rb
+++ spec/features/lessons/index_spec.rb
@@ -20,9 +20,19 @@ RSpec.describe 'lesson index page' do
         lesson1 = course1.lessons.create!(name:"Thinking about thinking", format:"text", questions:3, complete: true)
 
         visit '/lessons'
-        save_and_open_page
 
         expect(page).to have_content(lesson1.format)
         expect(page).to have_content("Format: #{lesson1.format}")
+    end 
+
+    it 'can show the number of questions' do 
+        course1 = Course.create!(name: "Basics", participants: 20, complete: false)
+        lesson1 = course1.lessons.create!(name:"Thinking about thinking", format:"text", questions:3, complete: true)
+
+        visit '/lessons'
+        save_and_open_page
+
+        expect(page).to have_content(lesson1.questions)
+        expect(page).to have_content("Questions: #{lesson1.questions}")
     end 
 end 
