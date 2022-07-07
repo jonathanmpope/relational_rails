60eaa07cb5f7675ed3eaf6eb8d8224b81130bc1f
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-07 11:44:14 -0600

    feat: add validation for name in course with test

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
     has_many :lessons
+    validates_presence_of :name
 
     def count 
         self.lessons.count
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Course , type: :model do 
     describe 'relationships' do 
-        
+
     end 
 
     describe 'validations' do
+         it { should validate_presence_of :name}
         
     end 
 
