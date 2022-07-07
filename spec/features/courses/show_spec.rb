6105d3d36ef3f726ebf596dabf50392866c05f5c
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-07 13:34:20 -0600

    feat: add course lessons link to course show page

--- spec/features/courses/show_spec.rb
+++ spec/features/courses/show_spec.rb
@@ -68,4 +68,17 @@ RSpec.describe 'Show page', type: :feature do
 
         expect(current_path).to eq('/courses')
     end
+
+    it 'has a link to the course lessons page' do 
+        course1 = Course.create!(name: "Basics", participants: 20, complete: false)
+        lesson1 = course1.lessons.create!(name:"Thinking about thinking", format:"text", questions:3, complete: true)
+        lesson2 = course1.lessons.create!(name:"Attention", format:"text", questions:3, complete: true)
+        lesson3 = course1.lessons.create!(name:"Trying is lying", format:"text", questions:2, complete: false)
+        lesson4 = course1.lessons.create!(name:"Quit tomorrow", format:"video", questions:1, complete: false)
+        
+        visit "/courses/#{course1.id}"
+        click_link('Course Lessons')
+
+        expect(current_path).to eq("/courses/#{course1.id}/lessons")
+    end
 end 
