4e0923beeff650cb1f58f7bb1cf9140ac84d5eed
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-07 11:36:57 -0600

    feat: add validation for lesson questions with test

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,5 @@ class Lesson < ApplicationRecord
     belongs_to :course
     validates_presence_of :name
     validates_presence_of :format
+    validates_presence_of :questions
 end 
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Lesson, type: :model do
     describe 'validations' do
         it { should validate_presence_of :name}
         it { should validate_presence_of :format}
+        it { should validate_presence_of :questions}
     end 
 
     describe 'instance methods' do 
