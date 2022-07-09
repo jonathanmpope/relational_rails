7219299fac450042a7d2e47b6a5970aef1a4d77d
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-09 08:54:44 -0600

    feat: add update lesson from course lessons index page with test

--- spec/features/courses/lessons/index_spec.rb
+++ spec/features/courses/lessons/index_spec.rb
@@ -109,4 +109,14 @@ RSpec.describe 'Course lesson index page' do
         
         expect(page).to have_current_path("/courses/#{course1.id}/lessons?sort=alphabet")
     end 
+
+    it 'has a link to update each lesson' do
+        course1 = Course.create!(name: "Basics", participants: 20, complete: false)
+        lesson1 = course1.lessons.create!(name:"Thinking about thinking", format:"text", questions:3, complete: true) 
+        
+        visit "/courses/#{course1.id}/lessons"
+        click_button("Update #{lesson1.name}")
+
+        expect(current_path).to eq("/lessons/#{lesson1.id}/edit")
+    end 
 end 
